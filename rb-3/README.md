40f5e14f331300c3c0090664c794a6c694105dc7
Rob Byrne <rob@leanrob.com>
2018-08-26 19:16:02 -0700

    Exploring borrowing and ownership
    - explore the ideas of ownsership and borrowing
    - explore using score to release ownership

--- rb-3/README.md
+++ rb-3/README.md
@@ -1,3 +1,3 @@
-# Day 3: CSV
+# Day 3: Ownership and Borrowing
 
-This project will be about working with and parsing CSV files.
+This project will be about working to understand and explore the ideas of ownership and borrowing.
--- rb-3/src/main.rs
+++ rb-3/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("CSV Stuff Coming Soon!");
+
+    // This function takes an immutable reference
+    let print_vector = |x:&Vec<i32>| {
+        println!("{:?}", x)
+    };
+
+    // Borrowing
+    let v = vec![3,2,1];
+
+    // & lets this function borrow this vector for a while
+    print_vector(&v);
+
+    // Now we can legally print this after it is used, because it was borrowed
+    println!("v[0] = {}", v[0]);
+
+    ////
+
+    // Note: There can only be 1 mutable reference to a resource
+    // - but there can be as many non-mutable references as we want
+    let mut a = 40;
+
+    // A new scope, so that when the scope ends b "release" ownership of a
+    {
+        // make a reference to a
+        let b = &mut a;
+
+        // * means the we want to use the thing that the reference references
+        *b += 2;
+    }
+
+    println!("a = {}", a);
 }
